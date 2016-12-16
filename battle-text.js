e520e546213a3a49dafdcfe9164371b8f09c1587
nazcat <nazli.uremek@gmail.com>
2016-12-16 17:03:28 -0500

    Update battle-text.js

--- battle-text.js
+++ battle-text.js
@@ -72,6 +72,9 @@ module.exports.userChoosePokemon = function(commandsArray) {
         text: textString,
         "spriteUrl": "<http://randompokemon.com/sprites/animated/{pkdx_id}.gif>"
         "unfurl_media": true
+            <meta property="og:url" content="http://randompokemon.com/sprites/animated/{pkdx_id}.gif"/>
+            <meta property=”og:site_name” content=”SwiftKey”/>
+            <meta property=”og:image” content="http://randompokemon.com/sprites/animated/{pkdx_id}.gif"/>
        }
     });    
   });
@@ -109,6 +112,9 @@ module.exports.npcChoosePokemon = function(dex_no) {
             text: textString,
             "spriteUrl": "<http://randompokemon.com/sprites/animated/{pkdx_id}.gif>"
             "unfurl_media": true
+                <meta property="og:url" content="http://randompokemon.com/sprites/animated/{pkdx_id}.gif"/>
+                <meta property=”og:site_name” content=”SwiftKey”/>
+                <meta property=”og:image” content="http://randompokemon.com/sprites/animated/{pkdx_id}.gif"/>
          }
     });    
   });
